1d4a50c3227250dcc1a15f9617732a121c922cf0
Clare <chblackburne@gmail.com>
2019-06-28 20:21:13 +0100

    use btn to generate random anecdote

--- 01/anecdotes/src/App.js
+++ 01/anecdotes/src/App.js
@@ -4,9 +4,18 @@ import './App.css';
 const App = (props) => {
   const [selected, setSelected] = useState(0)
 
+  const generateRandomAnecdote = (list) => {
+    const randomNum = Math.floor(Math.random() * Math.floor(list.length))
+    if(randomNum === selected){
+      return generateRandomAnecdote(list)
+    }
+    setSelected(randomNum)
+  }
+
   return (
     <div>
       {props.anecdotes[selected]}
+      <button onClick={() => generateRandomAnecdote(props.anecdotes)}>Get Anecdote</button>
     </div>
   )
 }
