c3d6acb5bcc41c9a3f3edd801733de522697031b
Clare <chblackburne@gmail.com>
2019-06-29 15:20:23 +0100

    implement highest vote function;

--- 01/anecdotes/src/App.js
+++ 01/anecdotes/src/App.js
@@ -1,21 +1,43 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const App = (props) => {
+const App = ({anecdotes}) => {
+  const stateObject = () => {
+    return anecdotes.map((anecdote, index) => {
+      return {
+        index,
+        vote: 0
+      }
+    })
+  }
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(stateObject)
+  const [highest, setHighest] = useState(0)
+
+  const getHighest = (scoresHash) => {
+    const copy = [...scoresHash]
+    return copy.reduce((previous, current) => {
+      return previous['vote'] > current['vote'] ? previous : current
+    })
+  }
 
   const generateRandomAnecdote = (list) => {
     const randomNum = Math.floor(Math.random() * Math.floor(list.length))
-    if(randomNum === selected){
-      return generateRandomAnecdote(list)
-    }
     setSelected(randomNum)
+    const copy = [...votes]
+    copy[randomNum]['vote'] += 1
+    setVotes(copy)
+    const highest = getHighest(copy)
+    setHighest(highest)
   }
 
   return (
     <div>
-      {props.anecdotes[selected]}
-      <button onClick={() => generateRandomAnecdote(props.anecdotes)}>Get Anecdote</button>
+      {anecdotes[selected]}
+      Number of Votes: {votes[selected]['vote']}
+      <button onClick={() => generateRandomAnecdote(anecdotes)}>Get Anecdote</button>
+      Anecdote with most votes:
+      {anecdotes[highest['index']]} Votes: {highest['vote']}
     </div>
   )
 }
