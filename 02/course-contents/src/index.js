1cb9364ed9fed162999cd8430fa6005163ca8c0f
Clare <chblackburne@gmail.com>
2019-06-30 18:58:45 +0100

    refactor

--- 02/course-contents/src/index.js
+++ 02/course-contents/src/index.js
@@ -3,4 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const courses = [
+{
+  name: 'Half Stack application development',
+  id: 1,
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+      id: 1
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+      id: 2
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+      id: 3
+    }
+  ]
+},
+{
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ]
+  }
+]
+
+ReactDOM.render(<App courses={courses} />, document.getElementById('root'));
