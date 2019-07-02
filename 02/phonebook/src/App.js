7bbee4a21326e9416f1a746d2169fc8398b62005
Clare <chblackburne@gmail.com>
2019-07-02 22:08:51 +0100

    basic search functionality

--- 02/phonebook/src/App.js
+++ 02/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchInput, setSearchInput ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -29,12 +33,31 @@ const App = () => {
   }
 
   const renderPeople = () => {
-    return persons.map(person => <p>{person.name} - {person.number}</p>)
+    if (searchInput === ''){
+      console.log('hello')
+      return persons.map(person => <p key={person.name}>{person.name} - {person.number}</p>)
+    } else {
+     return persons
+      .filter(person => {
+        if (person.name.toUpperCase().match(searchInput.toUpperCase())){
+          return person
+        }
+      })
+      .map(person => <p key={person.name}>{person.name} - {person.number}</p>)
+    }
+  }
+
+  const showNames =(event) => {
+    console.log(event.target.value)
+    setSearchInput(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <label>Filter shown with:</label>
+      <input onChange={showNames} value={searchInput}/>
+      <h2>Add a New </h2>
       <form
         onSubmit={addPerson}>
         <div>
