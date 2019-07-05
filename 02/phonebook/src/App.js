b9c719c04a6beafbe2f6eb162e78c18170986b3a
Clare <chblackburne@gmail.com>
2019-07-05 06:37:43 +0100

    unsaved changes

--- 02/phonebook/src/App.js
+++ 02/phonebook/src/App.js
@@ -12,15 +12,12 @@ const App = () => {
   const [ searchInput, setSearchInput ] = useState('')
 
   useEffect(() => {
-    console.log('effect')
     axios
       .get('http://localhost:3001/persons')
       .then(response => {
-        console.log('promise fulfilled')
         setPersons(response.data)
       })
   }, [])
-  console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
