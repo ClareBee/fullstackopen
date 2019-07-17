c82f49cd427409a5d144cc02d6f599d6ab744c7d
Clare <chblackburne@gmail.com>
2019-07-17 19:57:26 +0100

    hook up with mongo db

--- 03/phonebook_backend/index.js
+++ 03/phonebook_backend/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
+
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -12,36 +15,6 @@ app.use(express.static('build'))
 app.use(cors())
 
 const baseUrl = '/api'
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
-const generateId = (max) => {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-const checkUniqueness = (name) => {
-  return persons.find(person => person.name.toUpperCase() === name.toUpperCase())
-}
 
 app.get(`${baseUrl}`, (req, res) => {
   res.send('<h1>Hello world</h1>')
@@ -53,18 +26,15 @@ app.get(`${baseUrl}/info`, (req, res) => {
 })
 
 app.get(`${baseUrl}/persons`, (req, res) => {
-  res.json(persons)
+  Person.find({}).then(people => {
+    res.json(people.map(person => person.toJSON()))
+  })
 })
 
 app.get(`${baseUrl}/persons/:id`, (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person){
-    res.json(person)
-  } else {
-    // end method = empty response
-    res.status(404).end()
-  }
+  Person.findById(req.params.id).then(person => {
+    res.json(person.toJSON())
+  })
 })
 
 app.delete(`${baseUrl}/persons/:id`, (req, res) => {
@@ -81,24 +51,18 @@ app.post(`${baseUrl}/persons`, (req, res) => {
      error: 'content missing'
    })
   }
-  const notUnique = checkUniqueness(body.name)
-  if(notUnique){
-    return res.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    date: new Date(),
-    id: generateId(500),
-  }
-  persons = persons.concat(person)
+    number: body.number
+  })
+  console.log(person)
+  person.save().then(savedPerson => {
+    res.json(savedPerson.toJSON())
+  })
   morgan.token('person', function(req, res) { return JSON.stringify(person) })
-  res.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
