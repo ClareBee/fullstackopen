1a2ebb5ed271b85c181252b23290ce47d5463522
Clare <chblackburne@gmail.com>
2019-07-20 17:28:32 +0100

    refactoring

---
+++ 04/blog_list/models/blog.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+mongoose.set('useFindAndModify', false)
+
+const blogSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+blogSchema.plugin(uniqueValidator)
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
