0607c9ae65691fdca8e0c1730eda337bcd784a5f
Clare <chblackburne@gmail.com>
2019-07-21 11:11:29 +0100

    add helper tests

--- 04/blog_list/tests/list_helper.test.js
+++ 04/blog_list/tests/list_helper.test.js
@@ -6,3 +6,54 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+})
+
+describe('favourite blogs', () => {
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Bob Says',
+      author: 'Bob',
+      url: 'http://www.example.com',
+      likes: 20,
+      __v: 0
+    },{
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  test('when list has more than one blog equals the most likes', () => {
+    const result = listHelper.favouriteBlogs(listWithThreeBlogs)
+    console.log(result)
+    expect(result).toBe(listWithThreeBlogs[1])
+  })
+})
--- 04/blog_list/utils/list_helper.js
+++ 04/blog_list/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((prev, curr) => {
+    return prev + curr.likes
+  }, 0)
+}
+
+const favouriteBlogs = (blogs) => {
+  return blogs.reduce((prev, curr) => {
+    return curr.likes > prev.likes ? curr : prev
+  })
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favouriteBlogs
 }
