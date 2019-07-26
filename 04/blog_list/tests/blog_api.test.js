6e550cac42fc45a4472f26c59f0720439ea3278a
Clare <chblackburne@gmail.com>
2019-07-26 20:47:41 +0100

    more tests

--- 04/blog_list/tests/blog_api.test.js
+++ 04/blog_list/tests/blog_api.test.js
@@ -12,6 +12,18 @@ test('blog list is returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('there is one blog', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body.length).toBe(1)
+})
+
+test('the first blog is by Clare', async () => {
+  const response = await api.get('/api/blogs')
+  console.log(response)
+  expect(response.body[0].author).toBe('Clare')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
