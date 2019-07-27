946d7936105ff834a571bb610d0afab516399c4d
Clare <chblackburne@gmail.com>
2019-07-27 07:30:09 +0100

    passing tests

--- 04/blog_list/tests/blog_api.test.js
+++ 04/blog_list/tests/blog_api.test.js
@@ -3,30 +3,17 @@ const supertest = require('supertest')
 const app = require('../app')
 // creates superagent object https://github.com/visionmedia/superagent
 const api = supertest(app)
-const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
-const initialBlogs = [
-  {
-    title: 'Testing Title',
-    author: 'Clare',
-    url: 'http://www.example.com/claresblog',
-    likes: 5,
-  },
-  {
-    title: 'Second Testing Title',
-    author: 'Bob',
-    url: 'http://www.example.com/bobsblog',
-    likes: 10,
-  },
-]
+const Blog = require('../models/blog')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  let blogObject = new Blog(initialBlogs[0])
+  let blogObject = new Blog(helper.initialBlogs[0])
   await blogObject.save()
 
-  blogObject = new Blog(initialBlogs[1])
+  blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
 })
 
@@ -40,7 +27,7 @@ test('blog list is returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body.length).toBe(initialBlogs.length)
+  expect(response.body.length).toBe(helper.initialBlogs.length)
 })
 
 test('a specific blogs is within the returned blogs', async () => {
@@ -51,6 +38,47 @@ test('a specific blogs is within the returned blogs', async () => {
   expect(authors).toContain(
     'Clare'
   )
+})
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'new',
+    author: 'Sally',
+    url: 'http://www.example.com/sallysblog',
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+
+  const authors = blogsAtEnd.map(n => n.author)
+  expect(authors).toContain(
+    'Sally'
+  )
+})
+
+test('blog without title is not added', async () => {
+  const newBlog = {
+    title: '',
+    author: 'Sally',
+    url: 'http://www.example.com/sallysblog',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
 })
 
 afterAll(() => {
---
+++ 04/blog_list/tests/test_helper.js
@@ -0,0 +1,33 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Testing Title',
+    author: 'Clare',
+    url: 'http://www.example.com/claresblog',
+    likes: 5,
+  },
+  {
+    title: 'Second Testing Title',
+    author: 'Bob',
+    url: 'http://www.example.com/bobsblog',
+    likes: 10,
+  },
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ title: 'soontoberemoved' })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
