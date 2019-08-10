b7a7d1ea9d57fd2860731db7e7b4a920d95172cd
Clare <chblackburne@gmail.com>
2019-08-10 11:21:39 +0100

    mock click test added

--- 04/bloglist-frontend/src/components/SimpleBlog.js
+++ 04/bloglist-frontend/src/components/SimpleBlog.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const SimpleBlog = ({ blog, onClick }) => (
   <div>
-    <div>
-      {blog.title} {blog.author}
+    <div className="blog">
+      {blog.title} - {blog.author}
     </div>
-    <div>
+    <div className="likes">
       blog has {blog.likes} likes
       <button onClick={onClick}>like</button>
     </div>
--- 04/bloglist-frontend/src/components/SimpleBlog.test.js
+++ 04/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import { render, cleanup } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+// import { prettyDOM } from '@testing-library/dom'
 import SimpleBlog from './SimpleBlog'
-
-afterEach(cleanup)
 
 test('renders content', () => {
   const blog = {
@@ -19,4 +17,37 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent(
     'Component testing is done with react-testing-library'
   )
+  //  prints HTML on console
+  // component.debug()
+
+
+  const div = component.container.querySelector('.blog')
+  // searches for smaller part of HTML
+  // console.log(prettyDOM(div))
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library - Test'
+  )
+
+  const likesDiv = component.container.querySelector('.likes')
+  expect(likesDiv).toHaveTextContent('10 likes')
+})
+
+test('clicking the button calls event handler once', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Tester',
+    likes: 10
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
