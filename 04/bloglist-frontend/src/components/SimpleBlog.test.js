702ee1f12224377e1ae1fce288538fa746ec710e
Clare <chblackburne@gmail.com>
2019-08-10 10:56:16 +0100

    add basic front end test

---
+++ 04/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, cleanup } from '@testing-library/react'
+import SimpleBlog from './SimpleBlog'
+
+afterEach(cleanup)
+
+test('renders content', () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Test',
+    likes: 10
+  }
+
+  const component = render(
+    <SimpleBlog blog={blog} />
+  )
+
+  expect(component.container).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
+})
