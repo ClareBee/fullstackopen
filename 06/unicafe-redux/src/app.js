5442bfca2dcfcb9702a928a7e8fd04619e72e0a1
Clare <chblackburne@gmail.com>
2019-08-15 17:08:58 +0100

    refactor app component

---
+++ 06/unicafe-redux/src/app.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const App = (props) => {
+  const store = props.store
+
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type: 'RESET'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>neutral</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+export default App
--- 06/unicafe-redux/src/index.js
+++ 06/unicafe-redux/src/index.js
@@ -2,49 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import reducer from './reducer'
+import App from './App'
 
 const store = createStore(reducer)
 
-const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-
-  const bad = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
-  }
-
-  const ok = () => {
-    store.dispatch({
-      type: 'OK'
-    })
-  }
-
-  const reset = () => {
-    store.dispatch({
-      type: 'RESET'
-    })
-  }
-
-  return (
-    <div>
-      <button onClick={good}>good</button>
-      <button onClick={ok}>neutral</button>
-      <button onClick={bad}>bad</button>
-      <button onClick={reset}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
-  )
-}
-
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
+  )
 }
 
 renderApp()
