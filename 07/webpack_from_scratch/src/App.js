f10eeec22dfc6ffe21b00f27e38bb132a0c9f31e
Clare <chblackburne@gmail.com>
2019-09-18 20:41:27 +0100

    turn config into fn

--- 07/webpack_from_scratch/src/App.js
+++ 07/webpack_from_scratch/src/App.js
@@ -1,9 +1,18 @@
-import React, {useState} from 'react'
-import axios from 'axios'
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([])
+  const url = 'http://localhost:3001/notes'
+  const notes = useNotes(url)
 
   const handleClick = () => {
     setCounter(counter + 1)
@@ -14,7 +23,9 @@ const App = () => {
     <div className="container">
       hello webpack {counter} clicks
       <button onClick={handleClick} >press</button>
+      <div>{notes.length} notes on server {url}</div>
     </div>
   )
 }
+
 export default App
