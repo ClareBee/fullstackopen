f603e0e68b7db8ba43febfa5e4a24a73aaa76ff2
Clare <chblackburne@gmail.com>
2019-09-18 20:12:30 +0100

    add babel polyfill for async await

--- 07/webpack_from_scratch/webpack.config.js
+++ 07/webpack_from_scratch/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const config = {
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js'
