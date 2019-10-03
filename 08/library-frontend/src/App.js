417c3dc60e45ae47008a00f6d9c458432da942c9
Clare <chblackburne@gmail.com>
2019-10-03 17:50:54 +0100

    components and dependencies

--- 08/library-frontend/src/App.js
+++ 08/library-frontend/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
 
-function App() {
+const App = () => {
+  const [page, setPage] = useState('authors')
+
   return (
-    <div className="App">
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- 08/library-frontend/src/index.js
+++ 08/library-frontend/src/index.js
@@ -3,4 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import ApolloClient, { gql } from 'apollo-boost'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const query = gql`
+{
+  allBooks  {
+    title
+  }
+}
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
 ReactDOM.render(<App />, document.getElementById('root'));
