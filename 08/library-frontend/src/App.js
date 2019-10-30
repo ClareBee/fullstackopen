60b1d18cb25516a1cfd830056e7d26f8b6f49cf8
Clare <chblackburne@gmail.com>
2019-10-30 20:50:29 +0000

    wip adding front end login w apollo

--- 08/library-frontend/src/App.js
+++ 08/library-frontend/src/App.js
@@ -1,26 +1,88 @@
 import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useMutation, useApolloClient } from '@apollo/react-hooks'
 
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 
+const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password)  {
+      value
+    }
+  }
+`
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const client = useApolloClient()
+
+  const [token, setToken] = useState(null)
   const [page, setPage] = useState('authors')
 
+
+  const handleError = (error) => {
+    setErrorMessage(error.graphQLErrors[0].message)
+    setTimeout(() => {
+     setErrorMessage(null)
+    }, 10000)
+  }
+
+  const [login] = useMutation(LOGIN, {
+    onError: handleError
+  })
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  const errorNotification = () => errorMessage &&
+    <div style={{ color: 'red' }}>
+      {errorMessage}
+    </div>
+
+  if (!token) {
+    return (
+      <div>
+        {errorNotification()}
+        <h2>Login</h2>
+        <LoginForm
+          login={login}
+          setToken={(token) => setToken(token)}
+        />
+      </div>
+    )
+  }
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        {!token && (
+          <button onClick={() => setPage('login')}>login</button>
+        )}
+        {token && (
+          <button onClick={logout}>logout</button>
+        )}
       </div>
 
-      <Authors show={ page === 'authors'} />
+      <Authors show={page === 'authors'} />
 
-      <Books show={ page === 'books'} />
+      <Books show={page === 'books'} />
 
       <NewBook show={page === 'add'} />
+
+      <LoginForm
+        show={page === 'login'}
+        login={login}
+        setToken={(token) => setToken(token)}
+      />
 
     </div>
   )
--- 08/library-frontend/src/index.js
+++ 08/library-frontend/src/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from "@apollo/react-hooks"
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    }
+  }
+})
+
+// link is how client contacts server w JWT from local storage in header if exists
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <ApolloProvider client={client} >
